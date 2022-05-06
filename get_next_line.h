4b535c72f6022ab5c1313d436b4d8c742c0eefd6
komeno <tkomeno@student.42tokyo.jp>
2022-05-06 05:11:07 -0300

    some header files and BUFFER_SIZE settings

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,16 @@
 /*   By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 07:23:11 by tkomeno           #+#    #+#             */
-/*   Updated: 2022/05/04 10:42:53 by tkomeno          ###   ########.fr       */
+/*   Updated: 2022/05/06 05:10:43 by tkomeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <fcntl.h>
 # include <limits.h>
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
@@ -23,7 +25,9 @@ typedef enum e_bool
 	true
 }		t_bool;
 
-# define BUFFER_SIZE 256
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 256
+# endif
 
 char	*get_next_line(int fd);
 
